c81b772ecfb4da37dc8a80dc41c8a758985b7884
krsobo <krsobo@ttu.ee>
2023-02-05 15:35:33 +0200

    add some interfaces

---
+++ src/main/java/com/example/flashcardsbackend/entities/User.java
@@ -0,0 +1,21 @@
+package com.example.flashcardsbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String nickname;
+    private String email;
+
+}
---
+++ src/main/java/com/example/flashcardsbackend/services/BasicService.java
@@ -0,0 +1,9 @@
+package com.example.flashcardsbackend.services;
+
+public interface BasicService {
+
+    public void create();
+    public void read();
+    public void update();
+    public void delete();
+}
